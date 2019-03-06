2065b0fb98bfab0739de79a3d904f64660818b9b
Lam Nhan <lamnhan92@gmail.com>
2019-03-07 00:47:10 +0700

    update backend test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,20 +4,12 @@ export default () => {
 
     const router = Sheetbase.Router;
 
-    const previewUrl = 'https://sheetbase.net/theme/simpleblog-angular';
-
     router.get('/', (req, res) => {
-        return res.html(
-            `<h1>Sheetbase Simpleblog</h1>
-            <p>Preview: <a href="${previewUrl}">${previewUrl}</a></p>`,
-        );
+        return res.html(`<h1>Sheetbase Backend</h1>`);
     });
 
     router.post('/', (req, res) => {
-        return res.success({
-            title: 'Sheetbase Simpleblog',
-            preview: previewUrl,
-        });
+        return res.success({ title: 'Sheetbase Backend' });
     });
 
 };
--- backend/tests/test.ts
+++ backend/tests/test.ts
@@ -1,6 +1,21 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
+import { Browser } from '@sheetbase/testing';
 
-describe('Test me', () => {
-    it('should do something');
+import * as App from '../src/index';
+
+const browser = new Browser(App);
+
+describe('Home routes', () => {
+
+    it('GET /', () => {
+        const { body } = browser.get('/');
+        expect(body).to.contain('Sheetbase Backend');
+    });
+
+    it('POST /', () => {
+        const { body } = browser.post('/');
+        expect(body.data).to.eql({ title: 'Sheetbase Backend' });
+    });
+
 });
